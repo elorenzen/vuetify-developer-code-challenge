d8ea424f6732f17c4300bfee23a8944ec8d08945
Eric Lorenzen <eric.lorenzen@gmail.com>
2025-03-06 21:47:18 -0800

    Replace footer content with dark/light mode switch field

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-footer height="40" app>
+    <v-switch
+      v-model="model"
+      :label="!model ? 'Dark Mode' : 'Light Mode'"
+      hide-details
+      inset
+    />
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+  import { ref, watch } from 'vue'
+  import { useTheme } from 'vuetify'
+  const theme = useTheme()
+
+  const model = ref(false)
+  watch(model, (newVal) => {
+    theme.global.name.value = newVal ? 'myCustomDarkTheme' : 'myCustomLightTheme'
+  })
+</script>
+
+<style scoped lang="sass">
+</style>
